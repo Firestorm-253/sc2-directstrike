2db7a101953f36ed9cd1c9f1f5e0b4b9e3da550a
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-07-09 17:42:39 +0200

    Replay implict as Dictionary

--- src/api.sc2-directstrike/DTOs/Replay.cs
+++ src/api.sc2-directstrike/DTOs/Replay.cs
@@ -5,4 +5,11 @@ public record Replay
     public int Id { get; init; }
     public DateTime GameTime { get; init; }
     public int[] ReplayPlayersIds { get; init; } = null!;
+    public static implicit operator Dictionary<string, object>(Replay replay)
+        => new()
+        {
+            { "Id", replay.Id },
+            { "GameTime", replay.GameTime.ToString("yyyy-dd-MM hh:mm:ss") },
+            //{ "ReplayPlayersIds", this.ReplayPlayersIds },
+        };
 }
