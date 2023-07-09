d705cdb03c59a9daa13b26bad29ed3b2da26cc3d
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-07-10 00:15:40 +0200

    ReplayPlayerControllerTests - GetById

---
+++ src/api.sc2-directstrike.tests/UnitTests/Controllers/ReplayPlayerController.cs
@@ -0,0 +1,28 @@
+﻿namespace api.sc2_directstrike.tests.UnitTests.Controllers;
+
+using api.sc2_directstrike.Controllers;
+
+public class ReplayPlayerControllerTests
+{
+    ReplayPlayerController replayPlayerController = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        this.replayPlayerController = new ReplayPlayerController();
+
+        Program.DbContext = new DbContext();
+        Program.ConnectDb(true);
+    }
+
+    [Test]
+    public void GetById()
+    {
+        string pkt = "read_test";
+        int id = 0;
+
+        var replayPlayer = this.replayPlayerController.GetById(pkt, id).GetAwaiter().GetResult();
+
+        Assert.IsNotNull(replayPlayer);
+    }
+}
