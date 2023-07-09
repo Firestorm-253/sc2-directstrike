3dbde4221276ea7aa66a3d42a71c10ad90caec9d
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-07-09 23:21:55 +0200

    PlayerControllerTests - GetById

---
+++ src/api.sc2-directstrike.tests/UnitTests/Controllers/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿namespace api.sc2_directstrike.tests.UnitTests.Controllers;
+
+using api.sc2_directstrike.Controllers;
+
+public class PlayerControllerTests
+{
+    PlayerController playerController = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        this.playerController = new PlayerController();
+        Program.DbContext = new DbContext();
+
+        Program.ConnectDb(true);
+    }
+
+    [Test]
+    public void GetById()
+    {
+        string pkt = "read_test";
+        int id = 0;
+
+        var player = this.playerController.GetById(pkt, id).GetAwaiter().GetResult();
+
+        Assert.IsNotNull(player);
+    }
+}
