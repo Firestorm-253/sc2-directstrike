372b1062320c3f51770c0b3742dc16954e5f8035
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-07-11 01:56:19 +0200

    record PostReplayPlayer

--- src/api.sc2-directstrike/DTOs/ReplayPlayer.cs
+++ src/api.sc2-directstrike/DTOs/ReplayPlayer.cs
@@ -1,14 +1,29 @@
-﻿namespace api.sc2_directstrike.DTOs;
+﻿using System.Text.Json;
+
+namespace api.sc2_directstrike.DTOs;
 
 public record ReplayPlayer
 {
-    //public int Id { get; init; }
+    public int Id { get; init; }
     public int PlayerId { get; init; }
     public int ReplayId { get; init; }
 
     public int Team { get; init; }
     public int Position { get; init; }
     public string Commander { get; init; } = null!;
+
+
+    public static implicit operator Dictionary<string, object>(ReplayPlayer replayPlayer)
+        => new()
+        {
+            { "Id", replayPlayer.Id },
+            { "PlayerId", replayPlayer.PlayerId },
+            { "ReplayId", replayPlayer.ReplayId },
+
+            { "Team", replayPlayer.Team },
+            { "Position", replayPlayer.Position },
+            { "Commander", replayPlayer.Commander },
+        };
 
     public static implicit operator ReplayPlayer?(Dictionary<string, object> entry)
     {
@@ -19,7 +34,7 @@ public record ReplayPlayer
 
         return new ReplayPlayer()
         {
-            //Id = (int)entry["Id"],
+            Id = (int)entry["Id"],
             PlayerId = (int)entry["PlayerId"],
             ReplayId = (int)entry["ReplayId"],
 
@@ -28,4 +43,22 @@ public record ReplayPlayer
             Commander = (string)entry["Commander"],
         };
     }
+}
+
+public record PostReplayPlayer
+{
+    public PostPlayer Player { get; init; } = null!;
+
+    public int Team { get; init; }
+    public int Position { get; init; }
+    public string Commander { get; init; } = null!;
+
+
+    public static implicit operator ReplayPlayer(PostReplayPlayer postReplayPlayer)
+        => new()
+        {
+            Team = postReplayPlayer.Team,
+            Position = postReplayPlayer.Position,
+            Commander = postReplayPlayer.Commander,
+        };
 }
