a26e4498112213cca00725505ff486c172c0ae25
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-07-12 03:35:29 +0200

    RatingController

---
+++ src/api.sc2-directstrike/Controllers/RatingController.cs
@@ -0,0 +1,34 @@
+﻿using api.sc2_directstrike.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.sc2_directstrike.Controllers;
+using Services;
+using System.Xml.Linq;
+
+[ApiController]
+[Route("{pkt}/" + NAME)]
+public class RatingController : ControllerBase
+{
+    const string NAME = "rating";
+
+    private readonly IServiceProvider serviceProvider;
+    
+    public RatingController(IServiceProvider serviceProvider)
+    {
+        this.serviceProvider = serviceProvider;
+    }
+
+    [HttpPost]
+    public async Task ReCalc(string pkt)
+    {
+        if (pkt.Length != 24)
+        {
+            throw new ArgumentException("ERROR: Invalid PKT length!");
+        }
+
+        using var scope = this.serviceProvider.CreateScope();
+        var ratingService = scope.ServiceProvider.GetRequiredService<RatingService>();
+
+        await ratingService.ReCalc(pkt);
+    }
+}
