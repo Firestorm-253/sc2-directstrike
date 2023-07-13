511bf82243aa3bd3ee3fbeecd33f15fb89b4ba29
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-07-14 01:09:18 +0200

    Gaussian/Maths

---
+++ src/sc2-directstrike.api/Services/RatingService/Maths/GaussianElo.cs
@@ -0,0 +1,33 @@
+﻿namespace sc2_directstrike.api.Services.Data.RatingService.Maths;
+
+public static class GaussianElo
+{
+    public static Gaussian GetRatingAfter(Gaussian rating,
+                                          int actualResult,
+                                          double prediction,
+                                          Gaussian matchDist,
+                                          double impact,
+                                          bool isLeaver,
+                                          RatingOptions ratingOptions)
+    {
+        double delta = (actualResult - prediction) * ratingOptions.EloK * impact;
+
+        var info = Gaussian.ByMeanDeviation(rating.Mean + delta, matchDist.Deviation);
+        var ratingAfter = rating * info;
+
+        if (isLeaver)
+        {
+            ratingAfter = Gaussian.ByMeanDeviation(ratingAfter.Mean, rating.Deviation);
+        }
+
+        return ratingAfter;
+    }
+
+    public static (double, Gaussian) PredictMatch(Gaussian a, Gaussian b, double deviationOffset = 0)
+    {
+        var subtraction = b - a;
+        var match = Gaussian.ByMeanDeviation(subtraction.Mean, subtraction.Deviation + deviationOffset);
+
+        return (match.CDF(0), match);
+    }
+}
